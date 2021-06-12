55c379efa4df18bfb5d9ed268e8f848d575fb53f
Umeh Amaka <amakaumeh01@gmail.com>
2021-06-12 10:40:03 +0100

    set up routes

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const app = express();
 const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use(helmet());
 app.use(morgan("common"));
 
 app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
     console.log('server running!');
